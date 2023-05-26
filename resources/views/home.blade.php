1e489dbfad7b7937ca9dc07b8bf7bc6254e41fc2
Daniel Llanda <east.danielteddyllandaa@gmail.com>
2023-05-26 09:38:02 +0700

    gewn ganda

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section('title')
+HOME
+@endsection
+
+@section('nav')
+<h1>WELCOME TO HOME PAGE</h1>
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="">LANDING</a>
+            <a href="">HOME</a>
+            <a href="">ABOUT</a>
+            <a href="">CONTACT</a>
+        </nav>
+    </header>
+
+    @yield('nav')
+    
+</body>
+</html>
